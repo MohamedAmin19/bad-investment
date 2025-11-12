bbcb5ba7b03ce49f8df8e5845d83c83da7c7d158
maielghandour <74146386+maielghandour@users.noreply.github.com>
2025-11-12 02:47:25 +0200

    Update navigation.ts

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -1,6 +1,6 @@
 export const mainMenuItems = [
-  { label: "Artists", href: "/#artists" },
-  { label: "Updates", href: "/#updates" },
+  { label: "Artists", href: "/artists" },
+  { label: "Updates", href: "/updates" },
   { label: "Tour", href: "/tour" },
   { label: "Contact us", href: "/contact" },
   { label: "Submit your music", href: "/submit" },
